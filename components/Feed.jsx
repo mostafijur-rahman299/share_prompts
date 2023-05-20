d92364f331ed11d7337ad6a1f34d40a10e173260
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-05-20 16:08:44 +0600

    Feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,55 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+"use client";
 
-export default Feed
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt_16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPost();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} hangleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
