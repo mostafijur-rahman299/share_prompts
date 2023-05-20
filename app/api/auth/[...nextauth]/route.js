e9e3b0f4e2aca4904c9f692c56e7ffb1e1c78fbf
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-05-20 09:25:25 +0600

    small changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
@@ -22,21 +23,25 @@ const handler = NextAuth({
       try {
         await connectToDB();
 
+        // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
 
+        // if not, create a new user and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.image,
-           });
+          });
         }
+        
+        return true;
       } catch (error) {
         console.log(error);
         return false;
       }
     },
-  }
+  },
 });
 
 export { handler as GET, handler as POST };
